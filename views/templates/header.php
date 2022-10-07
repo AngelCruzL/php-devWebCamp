e80604e88e843e66c579c5ed7b5e95dde987d9a3
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 09:12:41 -0500

    feat: add header

---
+++ views/templates/header.php
@@ -0,0 +1,19 @@
+<header class="header">
+	<div class="header__container">
+		<nav class="header__navigation">
+			<a href="/registro" class="header__link">Registro</a>
+			<a href="/login" class="header__link">Iniciar Sesión</a>
+		</nav>
+
+		<div class="header__content">
+			<a href="/">
+				<h1 class="header__logo">&#60;DevWebCamp /></h1>
+			</a>
+
+			<p class="header__text">Octubre 7 y 8 - 2023</p>
+			<p class="header__text header__text--modality">En Línea - Presencial</p>
+
+			<a href="/" class="header__button">Comprar Pase</a>
+		</div>
+	</div>
+</header>
