93ec18ac4c0a486a1e8098035ace8d095a219423
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 08:10:42 -0500

    feat: add login page

--- views/auth/login.php
+++ views/auth/login.php
@@ -1,1 +1,23 @@
-<h1><?php echo $pageTitle; ?></h1>
+<main class="auth">
+	<h2 class="auth__heading"><?php echo $pageTitle; ?></h2>
+	<p class="auth__text">Inicia Sesión en DevWebCamp</p>
+
+	<form class="form">
+		<div class="form__field">
+			<label for="email" class="form__label">Correo Electrónico</label>
+			<input type="email" class="form__input" placeholder="correo@correo.com" id="email" name="email">
+		</div>
+
+		<div class="form__field">
+			<label for="password" class="form__label">Contraseña</label>
+			<input type="password" class="form__input" placeholder="Tu contraseña" id="password" name="password">
+		</div>
+
+		<input type="submit" class="form__submit" value="Iniciar Sesión">
+	</form>
+
+	<div class="actions">
+		<a href="/registro" class="actions__link">¿Aún no tienes cuenta? Obtener una</a>
+		<a href="/olvide" class="actions__link">¿Olvidaste tu contraseña?</a>
+	</div>
+</main>
