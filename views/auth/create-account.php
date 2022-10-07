62b7d022dda05db02080d9aac823d99e2a3f0500
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 18:43:53 -0500

    feat: add createAccount functionality

--- views/auth/create-account.php
+++ views/auth/create-account.php
@@ -2,20 +2,22 @@
 	<h2 class="auth__heading"><?php echo $pageTitle; ?></h2>
 	<p class="auth__text">Regístrate en DevWebCamp</p>
 
-	<form class="form">
+	<?php require_once __DIR__ . '/../templates/alerts.php'; ?>
+
+	<form class="form" method="POST">
 		<div class="form__field">
-			<label for="firstName" class="form__label">Nombre(s)</label>
-			<input type="text" class="form__input" placeholder="Tu nombre" id="firstName" name="firstName">
+			<label for="first_name" class="form__label">Nombre(s)</label>
+			<input type="text" class="form__input" placeholder="Tu nombre" id="first_name" name="first_name" value="<?php echo $user->first_name; ?>">
 		</div>
 
 		<div class="form__field">
-			<label for="lastName" class="form__label">Apellido</label>
-			<input type="text" class="form__input" placeholder="Apellido" id="lastName" name="lastName">
+			<label for="last_name" class="form__label">Apellido</label>
+			<input type="text" class="form__input" placeholder="Apellido" id="last_name" name="last_name" value="<?php echo $user->last_name; ?>">
 		</div>
 
 		<div class="form__field">
 			<label for="email" class="form__label">Correo Electrónico</label>
-			<input type="email" class="form__input" placeholder="correo@correo.com" id="email" name="email">
+			<input type="email" class="form__input" placeholder="correo@correo.com" id="email" name="email" value="<?php echo $user->email; ?>">
 		</div>
 
 		<div class="form__field">
