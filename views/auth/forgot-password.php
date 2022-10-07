a151167f6c0ad59f32810c5e1d2c0fdafef08a91
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 08:25:04 -0500

    feat: add forgot password page

---
+++ views/auth/forgot-password.php
@@ -0,0 +1,18 @@
+<main class="auth">
+	<h2 class="auth__heading"><?php echo $pageTitle; ?></h2>
+	<p class="auth__text">Recupera tu acceso a DevWebCamp</p>
+
+	<form class="form">
+		<div class="form__field">
+			<label for="email" class="form__label">Correo Electrónico</label>
+			<input type="email" class="form__input" placeholder="correo@correo.com" id="email" name="email">
+		</div>
+
+		<input type="submit" class="form__submit" value="Enviar Instrucciones">
+	</form>
+
+	<div class="actions">
+		<a href="/login" class="actions__link">¿Ya tienes una cuenta? Inicia Sesión</a>
+		<a href="/registro" class="actions__link">¿Aún no tienes cuenta? Obtener una</a>
+	</div>
+</main>
