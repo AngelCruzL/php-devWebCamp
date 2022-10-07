7aab164c8e2110f18a177684a1868e391ff4af50
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 15:33:57 -0500

    feat: add footer

---
+++ views/templates/footer.php
@@ -0,0 +1,18 @@
+<footer class="footer">
+	<div class="footer__grid">
+		<div class="footer__content">
+			<h3 class="footer__logo">&#60;DevWebCamp /></h3>
+
+			<p class="footer__text">
+				DevWebCamp es una conferencia para desarrolladores de todos los niveles, se lleva a cabo de forma presencial y en línea.
+			</p>
+		</div>
+	</div>
+
+	<p class="footer__copyright">
+		DevWebCamp
+		<span class="footer__copyright--regular">
+			&copy; - Todos los derechos reservados <?php echo date("Y"); ?>
+		</span>
+	</p>
+</footer>
