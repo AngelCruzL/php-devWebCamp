511d4d37463759340d83438afa52393963dddc54
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 23:38:48 -0500

    feat: add sidebar styles

---
+++ views/templates/admin-sidebar.php
@@ -0,0 +1,28 @@
+<aside class="dashboard__sidebar">
+	<nav class="dashboard__menu">
+		<a href="/admin/dashboard" class="dashboard__link">
+			<i class="fa-solid fa-house dashboard__icon"></i>
+			<span class="dashboard__menuText">Inicio</span>
+		</a>
+
+		<a href="/admin/ponentes" class="dashboard__link">
+			<i class="fa-solid fa-microphone dashboard__icon"></i>
+			<span class="dashboard__menuText">Ponentes</span>
+		</a>
+
+		<a href="/admin/eventos" class="dashboard__link">
+			<i class="fa-solid fa-calendar dashboard__icon"></i>
+			<span class="dashboard__menuText">Eventos</span>
+		</a>
+
+		<a href="/admin/registrados" class="dashboard__link">
+			<i class="fa-solid fa-users dashboard__icon"></i>
+			<span class="dashboard__menuText">Registrados</span>
+		</a>
+
+		<a href="/admin/regalos" class="dashboard__link">
+			<i class="fa-solid fa-gift dashboard__icon"></i>
+			<span class="dashboard__menuText">Regalos</span>
+		</a>
+	</nav>
+</aside>
