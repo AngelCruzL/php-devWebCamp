b6cb96860fccc28f7c11a0688d867239d42083a2
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-07 23:15:09 -0500

    feat: add header on admin panel

---
+++ views/templates/admin-header.php
@@ -0,0 +1,13 @@
+<header class="dashboard__header">
+	<div class="dashboard__headerGrid">
+		<a href="/">
+			<h2 class="dashboard__logo">&#60;DevWebCamp /></h2>
+		</a>
+
+		<nav class="dashboard__nav">
+			<form class="dashboard__form" method="POST" action="/logout">
+				<input type="submit" value="Cerrar Sesión" class="dashboard__submit--logout">
+			</form>
+		</nav>
+	</div>
+</header>
