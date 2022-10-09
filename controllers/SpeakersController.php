7e678e58f628166aadc27f54e6a83b038b854c11
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-08 22:19:40 -0500

    feat: add speakers controller and update routes

---
+++ controllers/SpeakersController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class SpeakersController
+{
+	public static function index(Router $router)
+	{
+		$router->render('admin/speakers/index', [
+			'pageTitle' => 'Ponentes / Conferencistas'
+		]);
+	}
+}
--- public/index.php
+++ public/index.php
@@ -4,6 +4,10 @@ require_once __DIR__ . '/../includes/app.php';
 
 use Controllers\AuthController;
 use Controllers\DashboardController;
+use Controllers\SpeakersController;
+use Controllers\EventsController;
+use Controllers\RegisteredController;
+use Controllers\GiftController;
 use MVC\Router;
 
 $router = new Router();
@@ -25,5 +29,13 @@ $router->get('/mensaje', [AuthController::class, 'message']);
 $router->get('/confirmar-cuenta', [AuthController::class, 'confirmAccount']);
 
 $router->get('/admin/dashboard', [DashboardController::class, 'index']);
+
+$router->get('/admin/ponentes', [SpeakersController::class, 'index']);
+
+$router->get('/admin/eventos', [EventsController::class, 'index']);
+
+$router->get('/admin/registrados', [RegisteredController::class, 'index']);
+
+$router->get('/admin/regalos', [GiftController::class, 'index']);
 
 $router->checkRoutes();
