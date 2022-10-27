1c01a8c467fd5df39002490b5921945b955623a3
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-27 17:02:06 -0500

    feat: sort events on agenda page controller

--- controllers/PublicPagesController.php
+++ controllers/PublicPagesController.php
@@ -2,6 +2,11 @@
 
 namespace Controllers;
 
+use Model\Category;
+use Model\Day;
+use Model\Event;
+use Model\Hour;
+use Model\Speaker;
 use MVC\Router;
 
 class PublicPagesController
@@ -27,10 +32,33 @@ class PublicPagesController
 		]);
 	}
 
-	public static function conferences(Router $router)
+	public static function agenda(Router $router)
 	{
-		$router->render('pages/conferences', [
-			'pageTitle' => 'Conferencias y Workshops'
+		$events = Event::order('hour_id', 'ASC');
+		$eventsFormatted = [];
+
+		foreach ($events as $event) {
+			$event->category = Category::find($event->category_id);
+			$event->day = Day::find($event->day_id);
+			$event->hour = Hour::find($event->hour_id);
+			$event->speaker = Speaker::find($event->speaker_id);
+
+			if ($event->day_id === '1' && $event->category_id === '1')
+				$eventsFormatted['workshops_day1'][] = $event;
+
+			if ($event->day_id === '2' && $event->category_id === '1')
+				$eventsFormatted['workshops_day2'][] = $event;
+
+			if ($event->day_id === '1' && $event->category_id === '2')
+				$eventsFormatted['conferences_day1'][] = $event;
+
+			if ($event->day_id === '2' && $event->category_id === '2')
+				$eventsFormatted['conferences_day2'][] = $event;
+		}
+
+		$router->render('pages/agenda', [
+			'pageTitle' => 'Conferencias y Workshops',
+			'events' => $eventsFormatted
 		]);
 	}
 }
