41a78e5697e108cad340f8c97aca2ebbe1ae1b07
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-26 23:22:00 -0500

    feat: add functionality to show events

--- controllers/EventsController.php
+++ controllers/EventsController.php
@@ -2,18 +2,42 @@
 
 namespace Controllers;
 
+use Classes\Pagination;
 use Model\Category;
 use Model\Day;
 use Model\Event;
 use Model\Hour;
+use Model\Speaker;
 use MVC\Router;
 
 class EventsController
 {
 	public static function index(Router $router)
 	{
+		if (!is_admin()) header('Location: /login');
+
+		$current_page = filter_var($_GET['page'], FILTER_VALIDATE_INT);
+		if (!$current_page || $current_page < 1) header('Location: /admin/eventos?page=1');
+
+		$registers_per_page = 10;
+		$total_registers = Event::total();
+
+		$pagination = new Pagination($current_page, $registers_per_page, $total_registers);
+		if ($pagination->total_pages() < $current_page) header('Location: /admin/eventos?page=1');
+
+		$events = Event::paginate($registers_per_page, $pagination->offset());
+
+		foreach ($events as $event) {
+			$event->category = Category::find($event->category_id);
+			$event->day = Day::find($event->day_id);
+			$event->hour = Hour::find($event->hour_id);
+			$event->speaker = Speaker::find($event->speaker_id);
+		}
+
 		$router->render('admin/events/index', [
-			'pageTitle' => 'Conferencias y Workshops'
+			'pageTitle' => 'Conferencias y Workshops',
+			'events' => $events,
+			'pagination' => $pagination->pagination()
 		]);
 	}
 
--- views/admin/events/index.php
+++ views/admin/events/index.php
@@ -6,3 +6,60 @@
 		Añadir Evento
 	</a>
 </div>
+
+<div class="dashboard__container">
+	<?php if (empty($events)) : ?>
+		<p class="text-center">No Hay Eventos Aún</p>
+	<?php else : ?>
+		<table class="table">
+			<thead class="table__thead">
+				<tr>
+					<th scope="col" class="table__th">Nombre</th>
+					<th scope="col" class="table__th">Categoría</th>
+					<th scope="col" class="table__th">Día y Hora</th>
+					<th scope="col" class="table__th">Ponente</th>
+					<th scope="col" class="table__th"></th>
+				</tr>
+			</thead>
+
+			<tbody class="table__tbody">
+				<?php foreach ($events as $event) : ?>
+					<tr class="table__tr">
+						<td class="table__td">
+							<?php echo $event->name; ?>
+						</td>
+
+						<td class="table__td">
+							<?php echo $event->category->name; ?>
+						</td>
+
+						<td class="table__td">
+							<?php echo $event->day->name . ', ' . $event->hour->hour; ?>
+						</td>
+
+						<td class="table__td">
+							<?php echo $event->speaker->first_name . ', ' . $event->speaker->last_name; ?>
+						</td>
+
+						<td class="table__td--actions">
+							<a class="table__action table__action--edit" href="/admin/ponentes/editar?id=<?php echo $event->id; ?>">
+								<i class="fa-solid fa-user-pen"></i>
+								Editar
+							</a>
+
+							<form action="/admin/ponentes/eliminar" method="POST" class="table__form">
+								<input type="hidden" name="id" value="<?php echo $event->id; ?>">
+								<button class="table__action table__action--delete" type="submit">
+									<i class="fa-solid fa-circle-xmark"></i>
+									Eliminar
+								</button>
+							</form>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	<?php endif; ?>
+</div>
+
+<?php echo $pagination; ?>
