15460387cf2b786820690aba5e71f739a6068084
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-27 10:52:24 -0500

    feat: add devwebcamp page

---
+++ views/pages/devwebcamp.php
@@ -0,0 +1,24 @@
+<main class="devwebcamp">
+	<h2 class="devwebcamp__heading"><?php echo $pageTitle; ?></h2>
+	<p class="devwebcamp__description">Conoce la conferencia más importante de Latinoamérica</p>
+
+	<div class="devwebcamp__grid">
+		<div class="devwebcamp__image">
+			<picture>
+				<source srcset="build/img/sobre_devwebcamp.avif" type="image/avif">
+				<source srcset="build/img/sobre_devwebcamp.webp" type="image/webp">
+				<img src="build/img/sobre_devwebcamp.jpg" alt="Imagen sobre DevWebCamp" loading="lazy">
+			</picture>
+		</div>
+
+		<div class="devwebcamp__content">
+			<p class="devwebcamp__text">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam dignissimos optio perspiciatis sint dolores labore corporis nostrum nam omnis, iusto amet totam praesentium deserunt harum? Autem voluptatibus aperiam voluptates. Unde.
+			</p>
+
+			<p class="devwebcamp__text">
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam dignissimos optio perspiciatis sint dolores labore corporis nostrum nam omnis, iusto amet totam praesentium deserunt harum? Autem voluptatibus aperiam voluptates. Unde.
+			</p>
+		</div>
+	</div>
+</main>
