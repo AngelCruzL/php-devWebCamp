4c5b947096f9157fba8d4bed20fd19699962628a
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-29 11:57:49 -0500

    fix: add if guard clauses

--- src/js/hours.js
+++ src/js/hours.js
@@ -1,93 +1,96 @@
 (function () {
 	const $hours = document.querySelector('#hours');
-	if (!$hours) return;
 
-	const $days = document.querySelectorAll('[name="day"]');
-	const $category = document.querySelector('[name="category_id"]');
-	const $inputHiddenDay = document.querySelector('[name="day_id"]');
-	const $inputHiddenHour = document.querySelector('[name="hour_id"]');
+	if ($hours) {
+		const $days = document.querySelectorAll('[name="day"]');
+		const $category = document.querySelector('[name="category_id"]');
+		const $inputHiddenDay = document.querySelector('[name="day_id"]');
+		const $inputHiddenHour = document.querySelector('[name="hour_id"]');
 
-	const search = {
-		category_id: +$category.value || '',
-		day: +$inputHiddenDay.value || '',
-	};
+		const search = {
+			category_id: +$category.value || '',
+			day: +$inputHiddenDay.value || '',
+		};
 
-	if (!Object.values(search).includes('')) {
-		(async () => {
-			await searchEvents();
+		if (!Object.values(search).includes('')) {
+			(async () => {
+				await searchEvents();
 
-			const id = $inputHiddenHour.value;
-			const $selectedHour = document.querySelector(`[data-hour-id="${id}"]`);
+				const id = $inputHiddenHour.value;
+				const $selectedHour = document.querySelector(`[data-hour-id="${id}"]`);
 
-			$selectedHour.classList.remove('hours__hour--disabled');
-			$selectedHour.classList.add('hours__hour--selected');
+				$selectedHour.classList.remove('hours__hour--disabled');
+				$selectedHour.classList.add('hours__hour--selected');
 
-			$selectedHour.onclick = selectHour;
-		})();
-	}
+				$selectedHour.onclick = selectHour;
+			})();
+		}
 
-	$category.addEventListener('change', searchTerm);
-	$days.forEach($day => $day.addEventListener('change', searchTerm));
+		$category.addEventListener('change', searchTerm);
+		$days.forEach($day => $day.addEventListener('change', searchTerm));
 
-	function searchTerm(e) {
-		search[e.target.name] = e.target.value;
+		function searchTerm(e) {
+			search[e.target.name] = e.target.value;
 
-		$inputHiddenHour.value = '';
-		$inputHiddenDay.value = '';
-		resetPreviousSelectedHour();
+			$inputHiddenHour.value = '';
+			$inputHiddenDay.value = '';
+			resetPreviousSelectedHour();
 
-		if (Object.values(search).includes('')) return;
+			if (Object.values(search).includes('')) return;
 
-		searchEvents();
-	}
+			searchEvents();
+		}
 
-	async function searchEvents() {
-		const { day, category_id } = search;
-		const url = `/api/horarios-eventos?day_id=${day}&category_id=${category_id}`;
-		const response = await fetch(url);
-		const events = await response.json();
+		async function searchEvents() {
+			const { day, category_id } = search;
+			const url = `/api/horarios-eventos?day_id=${day}&category_id=${category_id}`;
+			const response = await fetch(url);
+			const events = await response.json();
 
-		getAvailableHours(events);
-	}
+			getAvailableHours(events);
+		}
 
-	function getAvailableHours(events) {
-		const $hours = document.querySelectorAll('#hours li');
-		$hours.forEach($hour => $hour.classList.add('hours__hour--disabled'));
+		function getAvailableHours(events) {
+			const $hours = document.querySelectorAll('#hours li');
+			$hours.forEach($hour => $hour.classList.add('hours__hour--disabled'));
 
-		const takenHours = events.map(event => event.hour_id);
-		const result = Array.from($hours).filter(
-			$hour => !takenHours.includes($hour.dataset.hourId)
-		);
-		result.forEach(hour => hour.classList.remove('hours__hour--disabled'));
+			const takenHours = events.map(event => event.hour_id);
+			const result = Array.from($hours).filter(
+				$hour => !takenHours.includes($hour.dataset.hourId)
+			);
+			result.forEach(hour => hour.classList.remove('hours__hour--disabled'));
 
-		addEventListenerToAvailableHours();
-	}
+			addEventListenerToAvailableHours();
+		}
 
-	function addEventListenerToAvailableHours() {
-		const $availableHours = document.querySelectorAll(
-			'#hours li:not(.hours__hour--disabled)'
-		);
-		$availableHours.forEach($hour =>
-			$hour.addEventListener('click', selectHour)
-		);
-	}
+		function addEventListenerToAvailableHours() {
+			const $availableHours = document.querySelectorAll(
+				'#hours li:not(.hours__hour--disabled)'
+			);
+			$availableHours.forEach($hour =>
+				$hour.addEventListener('click', selectHour)
+			);
+		}
 
-	function selectHour(e) {
-		resetPreviousSelectedHour();
+		function selectHour(e) {
+			resetPreviousSelectedHour();
 
-		if (e.target.classList.contains('hours__hour--disabled')) return;
+			if (e.target.classList.contains('hours__hour--disabled')) return;
 
-		e.target.classList.add('hours__hour--selected');
-		$inputHiddenHour.value = e.target.dataset.hourId;
+			e.target.classList.add('hours__hour--selected');
+			$inputHiddenHour.value = e.target.dataset.hourId;
 
-		$inputHiddenDay.value = document.querySelector(
-			'[name="day"]:checked'
-		).value;
-	}
+			$inputHiddenDay.value = document.querySelector(
+				'[name="day"]:checked'
+			).value;
+		}
 
-	function resetPreviousSelectedHour() {
-		const $prevSelectedHour = document.querySelector('.hours__hour--selected');
-		if ($prevSelectedHour)
-			$prevSelectedHour.classList.remove('hours__hour--selected');
+		function resetPreviousSelectedHour() {
+			const $prevSelectedHour = document.querySelector(
+				'.hours__hour--selected'
+			);
+			if ($prevSelectedHour)
+				$prevSelectedHour.classList.remove('hours__hour--selected');
+		}
 	}
 })();
--- src/js/register.js
+++ src/js/register.js
@@ -3,70 +3,74 @@ import Swal from 'sweetalert2';
 (function () {
 	let events = [];
 	const $registerResume = document.querySelector('#register-resume');
-	const $eventsButtons = document.querySelectorAll('.event__add');
 
-	$eventsButtons.forEach(button =>
-		button.addEventListener('click', selectEvent)
-	);
+	if ($registerResume) {
+		const $eventsButtons = document.querySelectorAll('.event__add');
 
-	function selectEvent({ target }) {
-		if (events.length < 5) {
-			target.disabled = true;
-			events = [
-				...events,
-				{
-					id: target.dataset.id,
-					name: target.parentElement.querySelector('.event__name').textContent,
-				},
-			];
+		$eventsButtons.forEach(button =>
+			button.addEventListener('click', selectEvent)
+		);
+
+		function selectEvent({ target }) {
+			if (events.length < 5) {
+				target.disabled = true;
+				events = [
+					...events,
+					{
+						id: target.dataset.id,
+						name: target.parentElement.querySelector('.event__name')
+							.textContent,
+					},
+				];
+
+				renderEvents();
+			} else {
+				Swal.fire({
+					title: 'Error',
+					text: 'Máximo 5 eventos por registro',
+					icon: 'error',
+					confirmButtonText: 'OK',
+				});
+			}
+		}
+
+		function renderEvents() {
+			cleanEvents();
+
+			if (events.length > 0) {
+				events.forEach(({ name, id }) => {
+					const $event = document.createElement('div');
+					$event.classList.add('register__event');
+
+					const $title = document.createElement('h3');
+					$title.classList.add('register__title');
+					$title.textContent = name;
+
+					const $removeBtn = document.createElement('button');
+					$removeBtn.classList.add('register__remove');
+					$removeBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
+					$removeBtn.onclick = () => deleteEvent(id);
+
+					$event.appendChild($title);
+					$event.appendChild($removeBtn);
+					$registerResume.appendChild($event);
+				});
+			}
+		}
+
+		function deleteEvent(id) {
+			events = events.filter(event => event.id !== id);
+
+			const $addBtn = document.querySelector(`[data-id="${id}"]`);
+			$addBtn.disabled = false;
 
 			renderEvents();
-		} else {
-			Swal.fire({
-				title: 'Error',
-				text: 'Máximo 5 eventos por registro',
-				icon: 'error',
-				confirmButtonText: 'OK',
-			});
 		}
-	}
 
-	function renderEvents() {
-		cleanEvents();
-
-		if (events.length > 0) {
-			events.forEach(({ name, id }) => {
-				const $event = document.createElement('div');
-				$event.classList.add('register__event');
-
-				const $title = document.createElement('h3');
-				$title.classList.add('register__title');
-				$title.textContent = name;
-
-				const $removeBtn = document.createElement('button');
-				$removeBtn.classList.add('register__remove');
-				$removeBtn.innerHTML = '<i class="fa-solid fa-trash"></i>';
-				$removeBtn.onclick = () => deleteEvent(id);
-
-				$event.appendChild($title);
-				$event.appendChild($removeBtn);
-				$registerResume.appendChild($event);
-			});
-		}
-	}
-
-	function deleteEvent(id) {
-		events = events.filter(event => event.id !== id);
-
-		const $addBtn = document.querySelector(`[data-id="${id}"]`);
-		$addBtn.disabled = false;
-
-		renderEvents();
-	}
-
-	function cleanEvents() {
-		while ($registerResume.firstChild) {
-			$registerResume.removeChild($registerResume.firstChild);
+		function cleanEvents() {
+			while ($registerResume.firstChild) {
+				$registerResume.removeChild($registerResume.firstChild);
+			}
 		}
 	}
 })();
