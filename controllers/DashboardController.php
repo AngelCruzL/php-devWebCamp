a73fc148510afbfa6124468653143656aa783af3
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-31 08:53:42 -0600

    feat: add blocks with useful information on admin dashboard page

--- controllers/DashboardController.php
+++ controllers/DashboardController.php
@@ -2,14 +2,37 @@
 
 namespace Controllers;
 
+use Model\Event;
+use Model\Register;
+use Model\User;
 use MVC\Router;
 
 class DashboardController
 {
+	private static $virtual_registry_income = 46.41;
+	private static $face2face_registry_income = 189.54;
+
 	public static function index(Router $router)
 	{
+		$registers = Register::get(5);
+		foreach ($registers as $register) {
+			$register->user = User::find($register->user_id);
+		}
+
+		$total_virtual_registers = Register::total('pack_id', 2);
+		$total_face2face_registers = Register::total('pack_id', 1);
+
+		$incomes = ($total_face2face_registers * self::$face2face_registry_income) + ($total_virtual_registers * self::$virtual_registry_income);
+
+		$events_with_less_registers = Event::orderWithLimit('available_places', 'ASC', 5);
+		$events_with_more_registers = Event::orderWithLimit('available_places', 'DESC', 5);
+
 		$router->render('admin/dashboard/index', [
-			'pageTitle' => 'Panel de Administración'
+			'pageTitle' => 'Panel de Administración',
+			'registers' => $registers,
+			'incomes' => $incomes,
+			'events_with_less_registers' => $events_with_less_registers,
+			'events_with_more_registers' => $events_with_more_registers
 		]);
 	}
 }
--- views/admin/dashboard/index.php
+++ views/admin/dashboard/index.php
@@ -1,1 +1,49 @@
 <h2 class="dashboard__heading"><?php echo $pageTitle; ?></h2>
+
+<main class="blocks">
+	<div class="blocks__grid">
+		<div class="block">
+			<h3 class="block__heading">Últimos Registros</h3>
+
+			<?php foreach ($registers as $register) : ?>
+				<div class="block__content">
+					<p class="block__text">
+						<?php echo $register->user->first_name . " " . $register->user->last_name; ?>
+					</p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="block">
+			<h3 class="block__heading">Ingresos</h3>
+			<p class="block__text--quantity">$ <?php echo $incomes; ?></p>
+		</div>
+
+
+		<div class="block">
+			<h3 class="block__heading">Eventos con Menos Lugares Disponibles</h3>
+
+			<?php foreach ($events_with_less_registers as $event) : ?>
+
+				<div class="block__content">
+					<p class="block__text">
+						<?php echo $event->name . " - " . $event->available_places . ' Disponibles'; ?>
+					</p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="block">
+			<h3 class="block__heading">Eventos con Más Lugares Disponibles</h3>
+
+			<?php foreach ($events_with_more_registers as $event) : ?>
+
+				<div class="block__content">
+					<p class="block__text">
+						<?php echo $event->name . " - " . $event->available_places . ' Disponibles'; ?>
+					</p>
+				</div>
+			<?php endforeach; ?>
+		</div>
+	</div>
+</main>
