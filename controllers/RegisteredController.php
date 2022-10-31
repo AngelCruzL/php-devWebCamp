7242d23e9a2f64f2c5ab36f0d8d78e4f0060e22f
Ángel Cruz <59263971+AngelCruzL@users.noreply.github.com>
2022-10-31 07:22:34 -0600

    feat: add registers list and paginate it

--- controllers/RegisteredController.php
+++ controllers/RegisteredController.php
@@ -2,14 +2,37 @@
 
 namespace Controllers;
 
+use Classes\Pagination;
+use Model\Pack;
+use Model\Register;
+use Model\User;
 use MVC\Router;
 
 class RegisteredController
 {
 	public static function index(Router $router)
 	{
+		if (!is_admin()) header('Location: /login');
+
+		$current_page = filter_var($_GET['page'], FILTER_VALIDATE_INT);
+		if (!$current_page || $current_page < 1) header('Location: /admin/registrados?page=1');
+
+		$registers_per_page = 10;
+		$total_registers = Register::total();
+
+		$pagination = new Pagination($current_page, $registers_per_page, $total_registers);
+		if ($pagination->total_pages() < $current_page) header('Location: /admin/registrados?page=1');
+
+		$registers = Register::paginate($registers_per_page, $pagination->offset());
+		foreach ($registers as $register) {
+			$register->user = User::find($register->user_id);
+			$register->pack = Pack::find($register->pack_id);
+		}
+
 		$router->render('admin/registered/index', [
-			'pageTitle' => 'Usuarios Registrados'
+			'pageTitle' => 'Usuarios Registrados',
+			'registers' => $registers,
+			'pagination' => $pagination->pagination()
 		]);
 	}
 }
--- views/admin/registered/index.php
+++ views/admin/registered/index.php
@@ -1,1 +1,44 @@
 <h2 class="dashboard__heading"><?php echo $pageTitle; ?></h2>
+
+<div class="dashboard__containerButton">
+	<a href="/admin/ponentes/crear" class="dashboard__button">
+		<i class="fa-solid fa-circle-plus"></i>
+		Añadir Ponente
+	</a>
+</div>
+
+<div class="dashboard__container">
+	<?php if (empty($registers)) : ?>
+		<p class="text-center">No Hay Ponentes Aún</p>
+	<?php else : ?>
+		<table class="table">
+			<thead class="table__thead">
+				<tr>
+					<th scope="col" class="table__th">Nombre</th>
+					<th scope="col" class="table__th">Email</th>
+					<th scope="col" class="table__th">Plan</th>
+				</tr>
+			</thead>
+
+			<tbody class="table__tbody">
+				<?php foreach ($registers as $register) : ?>
+					<tr class="table__tr">
+						<td class="table__td">
+							<?php echo $register->user->first_name . " " . $register->user->last_name; ?>
+						</td>
+
+						<td class="table__td">
+							<?php echo $register->user->email; ?>
+						</td>
+
+						<td class="table__td">
+							<?php echo $register->pack->name; ?>
+						</td>
+					</tr>
+				<?php endforeach; ?>
+			</tbody>
+		</table>
+	<?php endif; ?>
+</div>
+
+<?php echo $pagination; ?>
